c37061099911ab07b9a9365cf5299f4855ac2fc4
Luis Villamil <luis.villamil@unosquare.com>
2021-09-23 11:49:44 -0500

    add composite excersie

---
+++ module1.2/composite/ex.js
@@ -0,0 +1,26 @@
+class SingleValue
+{
+  constructor(value)
+  {
+    this.values = [value];
+  }
+}
+
+class ManyValues
+{
+    // ensure there's a push(value) method
+    constructor() {
+      this.values = [];
+    }
+    
+    push(value) {
+        this.values.push(value);
+    }
+}
+
+let sum = function(containers)
+{
+  return containers.reduce((x, y) => { 
+      return x + y.values.reduce((xa, ya) => ya + xa) }
+      , 0)
+};
