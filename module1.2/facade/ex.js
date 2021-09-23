34e20a7dfee1a1087d4f494795127045f3907757
Luis Villamil <luis.villamil@unosquare.com>
2021-09-23 14:40:24 -0500

    add facade exercise

---
+++ module1.2/facade/ex.js
@@ -0,0 +1,49 @@
+class Bird
+{
+  constructor(age=0)
+  {
+    this.age = age;
+  }
+
+  fly()
+  {
+    return this.age < 10 ? 'flying' : 'too old';
+  }
+}
+
+class Lizard
+{
+  constructor(age=0)
+  {
+    this.age = age;
+  }
+
+  crawl()
+  {
+    return this.age > 1 ? 'crawling' : 'too young';
+  }
+}
+
+class Dragon
+{
+    constructor(age = 0) {
+		this.bird = new Bird();
+		this.lizard = new Lizard();
+		this._age = age;
+	}
+
+	set age(value) {
+		this._age = this.bird.age = this.lizard.age = value;
+	}
+
+	get age() {
+		return this._age;
+	}
+
+	fly() {
+		return this.bird.fly();
+	}
+	crawl() {
+		return this.lizard.crawl();
+	}
+}
