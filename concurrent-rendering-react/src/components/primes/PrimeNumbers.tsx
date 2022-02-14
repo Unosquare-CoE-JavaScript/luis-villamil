75f699f0a37be40c7479101dab3abb2bca77b648
Luis Villamil <luis.villamil@unosquare.com>
2022-02-14 11:08:41 -0600

    test deffered value for prime number generator

--- concurrent-rendering-react/src/components/primes/PrimeNumbers.tsx
+++ concurrent-rendering-react/src/components/primes/PrimeNumbers.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import { CheckNumber } from './CheckNumber';
 import { PrimeRange } from './PrimeRange';
 
@@ -7,20 +7,20 @@ const defaultValue = 250;
 export function PrimeNumbers() {
   const [maxPrime, setMaxPrime] = useState(defaultValue);
   const values = new Array(maxPrime).fill(null);
-
+  const [isPending, startTransition ] =useTransition();
   return (
     <div className="row">
       <h2 className="text-center mt-5">Prime Numbers</h2>
       <PrimeRange
         defaultValue={defaultValue}
-        onChange={(value) => setMaxPrime(value)}
+        onChange={(value) => setMaxPrime(value)} // startTransition is faster than deferring the value
       />
 
       <div className="row row-cols-auto g-2">
         {values
           .filter((_, index) => index < 10_000)
           .map((_, index) => {
-            return <CheckNumber key={index} value={maxPrime - index} />;
+            return <CheckNumber key={index} value={maxPrime - index} isPending={isPending}/>;
           })}
       </div>
     </div>
