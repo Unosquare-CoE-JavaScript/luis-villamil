cd854f487b9265db1c50154a926dc3ca39bbfa75
Luis Villamil <luis.villamil@unosquare.com>
2022-02-14 11:08:21 -0600

    feat: use suspense on main app

--- concurrent-rendering-react/src/index.tsx
+++ concurrent-rendering-react/src/index.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ErrorBoundary } from 'react-error-boundary';
-import { SWRConfig } from 'swr';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { ErrorBoundary } from "react-error-boundary";
+import { SWRConfig } from "swr";
 
-import 'bootstrap/dist/css/bootstrap.css';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
-import { ErrorFallback } from './components/ErrorFallback';
-import { fetcher } from './utils/fetcher';
+import App from "./App";
+import { ErrorFallback } from "./components/ErrorFallback";
+import { fetcher } from "./utils/fetcher";
+import { Loading } from "./components/Loading";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <React.StrictMode>
-      <SWRConfig value={{ fetcher }}>
-        <App />
-      </SWRConfig>
-    </React.StrictMode>
+    <Suspense fallback={<Loading />}>
+      <React.StrictMode>
+        <SWRConfig value={{ fetcher, suspense: true }}>
+          <App />
+        </SWRConfig>
+      </React.StrictMode>
+    </Suspense>
   </ErrorBoundary>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
